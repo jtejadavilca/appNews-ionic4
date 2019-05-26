afcb0e916e0a45e01db564b885a2f5ed4674cb25
Jose Tejada <jtejadavilca@gmail.com>
2019-05-26 01:58:09 -0500

    Se agrega plugin InAppBrowser

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../pages/interfaces/interfaces';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -11,8 +13,12 @@ export class NoticiaComponent implements OnInit {
   @Input() i: number;
   @Input() noticia: Article;
 
-  constructor() { }
+  constructor(private iab: InAppBrowser) { }
 
   ngOnInit() {}
 
+  abrirNoticia() {
+
+    const browser = this.iab.create( this.noticia.url, '_system');
+  }
 }
