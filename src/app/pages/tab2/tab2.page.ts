e0e863d29938c81256e59c3e1724a15a445fc57f
Jose Tejada <jtejadavilca@gmail.com>
2019-05-26 00:52:20 -0500

    Carga de noticias por categoria

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
+import { NoticiasService } from '../../services/noticias.service';
+import { Article } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -11,10 +13,23 @@ export class Tab2Page implements OnInit {
   @ViewChild(IonSegment) segment: IonSegment;
 
   categorias = [ 'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology' ];
+  noticias: Article[] = [];
 
-  constructor() {}
+  constructor(private noticiasService: NoticiasService) {}
 
   ngOnInit() {
     this.segment.value = this.categorias[0];
+    this.cargarNoticiasPorCategoria(this.categorias[0]);
+  }
+
+  cambiarCategoria(event) {
+    this.cargarNoticiasPorCategoria(event.detail.value);
+  }
+  cargarNoticiasPorCategoria(categoria) {
+    this.noticiasService.getTopHeadlinesCategoria(categoria)
+    .subscribe( resp => {
+      this.noticias = [];
+      this.noticias.push(...resp.articles);
+    } );
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiKey: '687ce7b5965542a6b95db406f7e4a25c'
+  apiKey: '687ce7b5965542a6b95db406f7e4a25c',
+  apiUrl: 'https://newsapi.org/v2'
 };
 
 /*
